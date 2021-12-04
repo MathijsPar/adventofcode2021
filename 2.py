de389d04f8a3979ce6534317f3161822f9e9ea24
unknown <mathijspar@gmail.com>
2021-12-04 12:46:24 +0100

    day 2

--- 2.py
+++ 2.py
@@ -1,2 +1,20 @@
+#1
 inp = ''.join(int(x[-1])*x[0] for x in open("in2.txt").read().strip().split('\n'))
-(inp.count('d')-inp.count('u'))*inp.count('f')
+print((inp.count('d')-inp.count('u'))*inp.count('f'))
+
+#2
+inp = [x.strip().split() for x in open("in2.txt")]
+coords = [0,0]
+aim = 0
+
+for d,m in inp:
+    m = int(m)
+    if d[0] == 'f':
+        coords[0] += aim*m
+        coords[1] += m
+    elif d[0] == 'd':
+        aim += m
+    elif d[0] == 'u':
+        aim -= m
+
+print(coords[0]*coords[1])
